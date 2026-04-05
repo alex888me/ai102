18c0ae90d4b4cd65bc5d1528e0211b5914aa2ad6
art <art@example.com>
2026-04-05 10:50:27 +0200

    Translate text and speech

---
+++ src/training3/module7/__init__.py
@@ -0,0 +1,2 @@
+"""Translate text and speech
+https://microsoftlearning.github.io/mslearn-ai-language/Instructions/Exercises/07-translation.html"""
---
+++ src/training3/module7/translate-speech.py
@@ -0,0 +1,67 @@
+import os
+from dotenv import load_dotenv
+
+
+# Import namespaces
+from azure.identity import DefaultAzureCredential
+import azure.cognitiveservices.speech as speech_sdk
+
+
+def main():
+    try:
+        # Configure translation
+        translation_cfg = speech_sdk.translation.SpeechTranslationConfig(
+            token_credential=DefaultAzureCredential(),
+            endpoint=os.getenv('FOUNDRY_ENDPOINT'),
+        )
+        translation_cfg.speech_recognition_language = 'en-US'
+        translation_cfg.add_target_language('fr')
+        translation_cfg.add_target_language('es')
+        translation_cfg.add_target_language('hi')
+        audio_in_cfg = speech_sdk.AudioConfig(use_default_microphone=True)
+        translator = speech_sdk.translation.TranslationRecognizer(
+            translation_config=translation_cfg,
+            audio_config=audio_in_cfg
+        )
+        print('Ready to translate from',translation_cfg.speech_recognition_language)
+
+
+        # Configure speech for synthesis of translations
+        speech_cfg = speech_sdk.SpeechConfig(
+            token_credential=DefaultAzureCredential(),
+            endpoint=os.getenv('FOUNDRY_ENDPOINT'),
+        )
+        audio_out_cfg = speech_sdk.audio.AudioOutputConfig(use_default_speaker=True)
+        voices = {
+            "fr": "fr-FR-HenriNeural",
+            "es": "es-ES-ElviraNeural",
+            "hi": "hi-IN-MadhurNeural"
+        }
+        print('Ready to use speech service.')
+
+        # Translate user speech
+        print("Speak now...")
+        translation_results = translator.recognize_once_async().get()
+        print(f"Translating '{translation_results.text}'")
+        
+        # Print and speak the translation results
+        translations = translation_results.translations
+        for translation_language in translations:
+
+            print(f"{translation_language}: '{translations[translation_language]}'")
+
+            speech_cfg.speech_synthesis_voice_name = voices.get(translation_language)
+            audio_out_cfg = speech_sdk.audio.AudioOutputConfig(use_default_speaker=True)
+            speech_synthesizer = speech_sdk.SpeechSynthesizer(speech_cfg, audio_out_cfg)
+            speak = speech_synthesizer.speak_text_async(translations[translation_language]).get()
+            
+            if speak.reason != speech_sdk.ResultReason.SynthesizingAudioCompleted:
+                print(speak.reason)
+
+
+    except Exception as ex:
+        print(ex)
+
+
+if __name__ == "__main__":
+    main()
